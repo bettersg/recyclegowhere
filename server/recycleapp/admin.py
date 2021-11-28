04d96efccfb5eac18c1772e451633b5ebe5d0829
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-11-28 11:17:00 +0800

    Register all the Data Classes

--- server/recycleapp/admin.py
+++ server/recycleapp/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from .models import ItemList
+from .models import Category, Items, CallForCollection, b, n, g, ReuseChannel, PhysicalChannels
 
 # Register your models here.
 
-admin.site.register(ItemList)
+admin.site.register(Category)
+admin.site.register(Items)
+admin.site.register(CallForCollection)
+admin.site.register(b)
+admin.site.register(n)
+admin.site.register(g)
+admin.site.register(ReuseChannel)
+admin.site.register(PhysicalChannels)
