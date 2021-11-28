8867efaf419d29762e56d1e36c29401158c7f9ee
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-11-28 11:05:28 +0800

    Add Django Models (Final)

--- server/recycleapp/models.py
+++ server/recycleapp/models.py
@@ -3,7 +3,6 @@ import urllib, json
 
 # Create your models here.
 
-
 class Category(models.Model):
     """
     Data Model for Categorising Items.
@@ -11,144 +10,117 @@ class Category(models.Model):
     Data Structure:
         str category
     """
-
     category = models.CharField(blank=True, null=True, max_length=100)
+
+
+class Items(models.Model):
+    """
+    Data Model for Items in General
+
+    Data Structure:
+        str examples/description
+        onetomany category
+        int bluebinrecyclable
+    """
+    description = models.CharField(blank=True, null=True, max_length=1000)
+    category = models.ForeignKey(Category)
+    bluebinrecyclable = models.IntegerField(blank=True, null=True)
+
+
+class b(models.Model):
+    """
+    Data Model for Blue Bin Recyclables
+
+    Data Structure:
+        int pk (predefined)
+        str category
+        str question
+    """
+    category = models.ForeignKey(Category)
+    question = models.CharField(blank=True, null=True, max_length=1000)
+
 
 class CallForCollection(models.Model):
     """
-    Data Model for Objects being Collected
-    
+    Data Model for Call For Collection
+
     Data Structure:
-        int pk
+        int pk (predefined)
         str name
         str items_collected
     """
     name = models.CharField(blank=True, null=True, max_length=100)
     items_collected = models.CharField(blank=True, null=True, max_length=1000)
 
-    
-class Item(models.Model):
+class n(models.Model):
     """
-    Data Model for Items to be recycled or alternatively handled.
+    Data Model for Non-Blue Bin Recyclables
 
     Data Structure:
-        str item
-        onetomany item_category
-        str material
-        int bluebinrecyclable
-    """
-    item = models.CharField(blank=True, null=True, max_length=100)
-    item_category = models.ForeignKey(Category)
-    material = models.CharField(blank=True, null=True, max_length=100)
-    bluebinrecyclable = models.IntegerField(blank=True, null=True)
-
-
-
-
-class CatHandle(models.Model):
-    """
-    Data Model for Handling Items based on Category
-    
-    Data Structure:
-        str item_category
-        str how_to_handle
-    """
-    item_category = models.CharField(blank=True, null=True, max_length=100)
-    how_to_handle = models.CharField(blank=True, null=True, max_length=1000)
-
-
-class CheckCondition(models.Model):
-    """
-    Data Model for Handling Non-Blue Bin Recyclables by Condition
-
-    Data Structure:
-        str condition_cat
+        str category
+        manytomany call_for_collection
         str in_good_condition
         str in_need_of_repair
         str spoilt_beyond_repair
     """
-    condition_cat = models.CharField(blank=True, null=True, max_length=100)
-    in_good_condition = models.CharField(blank=True, null=True, max_length=100)
-    in_need_of_repair = models.CharField(blank=True, null=True, max_length=100)
-    spoilt_beyond_repair = models.CharField(blank=True, null=True, max_length=100)
+    category = models.ForeignKey(Category)
+    call_for_collection = models.ManyToManyField(CallForCollection)
+    in_good_condition = models.CharField(blank=True, null=True, max_length=1000)
+    in_need_of_repair = models.CharField(blank=True, null=True, max_length=1000)
+    spoilt_beyond_repair = models.CharField(blank=True, null=True, max_length=1000)
+
+class g(models.Model):
+    """
+    Data Model for General Waste
+
+    Data Structure:
+        int pk (predefined)
+        str category
+        str reason
+        str suggestion
+    """
+    category = models.ForeignKey(Category)
+    reason = models.CharField(blank=True, null=True, max_length=1000)
+    suggestion = models.CharField(blank=True, null=True, max_length=1000)
+
 
 class ReuseChannel(models.Model):
     """
     Data Model for Handling Reuse Channels
-    """
-    channel_of_reuse = models.CharField(blank=True, null=True, max_length=100)
-
-class PhysicalChannel(models.Model):
-    """
-    Data Model for Physical Channels (from MSE and Alternative Sources)
-    """
-    name_of_organisation = models.CharField(
-        blank=True, null=True, max_length=100)
-    address = models.CharField(blank=True, null=True, max_length=1000)
-    blocknumber = models.CharField(blank=True, null=True, max_length=10)
-    building_name = models.CharField(blank=True, null=True, max_length=100)
-    postcode = models.IntegerField(blank=True, null=True)
-    operating_hours = models.CharField(blank=True, null=True, max_length=1000)
-    contact = models.IntegerField(blank=True, null=True)
-    category = models.ForeignKey(Category)
-    channel_of_reuse = models.ForeignKey(ReuseChannel)
-    remarks = models.CharField(blank=True, null=True, max_length=1000)
-
-
-# def getLatLong(postalCode): return tuple((lambda dict: map(float, map(dict.get, ["LATITUDE", "LONGITUDE"])))(json.loads(urllib.request.urlopen(f"https://developers.onemap.sg/commonapi/search?searchVal={postalCode}&returnGeom=Y&getAddrDetails=Y&pageNum=1").read())["results"][0]))
-
-class Item(models.Model):
-    """
-    Abstract Data Model for Items to be Recycled or Disposed.
 
     Data Structure:
-        int pk
-        str name
-        int bluebinrecyclable
-    
-    Example of Data: (10, "Medicine Glassware", 2)
+        str channel_of_reuse
+
+    Choices:
+        channel_of_reuse: Donate, Resell, Repair
     """
-    name = models.CharField(blank=True, null=True, max_length=100)
-    bluebinrecyclable = models.IntegerField(blank=True, null=True)
+    CHANNEL_CHOICES = [
+        ("DONATE", "Donate"),
+        ("RESELL", "Resell"),
+        ("REPAIR", "Repair")
+    ]
+    channel_of_reuse = models.CharField(blank=True, null=True, max_length=100, choices=CHANNEL_CHOICES)
 
-    class Meta:
-        abstract = True
 
-class BlueBinRecyclable(Item):
+class PhysicalChannels(models.Model):
     """
-    Data Model for Items that can be Recycled via the Blue Bins
-
-    Note: Inherits from Item Class, hence contains some of the pre-existing arguments
+    Data Model for Physical Channels
 
     Data Structure:
-        int pk
-        str name
-        int bluebinrecyclable = 0
-        str how_to_handle
-    
+        int s/n
+        str name_of_organisation
+        str address
+        str blocknumber
+        str building_name
+        int postcode
+        str operating_hours
+        int contact
+        slug website
+        onetomany category
+        onetomany channel_of_reuse
+        str remarks
     """
-    bbr = 0
-    how_to_handle = models.CharField(blank=True, null=True, max_length=1000)
-
-class NonBlueBinRecyclable(Item):
-    bbr = 1
-    category = models.ForeignKey(Category)
-
-class GeneralWaste(Item):
-    bbr = 2
-    #name = models.CharField(blank=True, null=True, max_length=100)
-    reason = models.CharField(blank=True, null=True, max_length=1000)
-    suggestions = models.CharField(blank=True, null=True, max_length=1000)
-
-class RecyclingLocation(models.Model):
-    latitude = models.IntegerField(blank=True, null=True)
-    longitude = models.IntegerField(blank=True, null=True)
-    location_descriptor = models.CharField(blank=True, null=True, max_length=1000)
-    item_idx = models.IntegerField(blank=True, null=True)
-    references = models.IntegerField(blank=True, null=True)
-    items = models.IntegerField(blank=True, null=True)
-
-class MSEPhysicalChannel(models.Model):
+    sn = models.IntegerField(blank=True, null=True)
     name_of_organisation = models.CharField(blank=True, null=True, max_length=100)
     address = models.CharField(blank=True, null=True, max_length=1000)
     blocknumber = models.CharField(blank=True, null=True, max_length=10)
@@ -156,4 +128,7 @@ class MSEPhysicalChannel(models.Model):
     postcode = models.IntegerField(blank=True, null=True)
     operating_hours = models.CharField(blank=True, null=True, max_length=1000)
     contact = models.IntegerField(blank=True, null=True)
+    website = models.SlugField(blank=True, null=True)
+    category = models.ForeignKey(Category)
+    channel_of_reuse = models.ForeignKey(ReuseChannel)
     remarks = models.CharField(blank=True, null=True, max_length=1000)
