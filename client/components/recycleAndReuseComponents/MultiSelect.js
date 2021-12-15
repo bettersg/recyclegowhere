6098916dbef16d9afb95d283175691552b0b90d8
joanneong <joanneongcf@u.nus.edu>
2021-12-15 20:33:23 +0800

    add: add VerifyItem component

--- client/components/recycleAndReuseComponents/MultiSelect.js
+++ client/components/recycleAndReuseComponents/MultiSelect.js
@@ -1,47 +1,76 @@
-import { Button, Flex } from "@chakra-ui/react";
-import React from "react";
-import Select from "react-select";
-import { options }  from '../../../mockData/data';
-import { useState } from "react";
+import { Button, Flex } from '@chakra-ui/react'
+import React from 'react'
+import Select from 'react-select'
+import { options }  from '../../../mockData/data'
 
+const MultiSelect = ({ data, setItems, willTriggerNotification }) => {
+  const [selectedOptions, setSelectedOptions] = React.useState([])
 
-const MultiSelect = ({ optionss = options, setInput, updateSelectedItems, updateTabIndex, willTriggerNotification }) => {
-
-    const [listItems, updateListItems] = useState();
-
-    const handleChange = (event) => {
-        updateListItems(event);
-    };
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-
-        // Safer logic
-        if (listItems !== undefined) {
-            updateSelectedItems(listItems);
-            willTriggerNotification(true);
+  const parseItemsIntoOptions = (categories, items) => {
+    const allOptions = []
+    categories.forEach(category => {
+      const itemsInCategory = items.filter(item => item.category === category)
+      const itemOptions = []
+      itemsInCategory.forEach(itemInCategory => {
+        const option = {
+          "label": itemInCategory.description,
+          "value": itemInCategory.id
         }
-        
-    };
+        itemOptions.push(option)
+      })
 
-    return (
-        <form onSubmit={handleSubmit}>
+      const categoryOptions = {
+        "label": category,
+        "options": itemOptions
+      }
+      allOptions.push(categoryOptions)
+    })
+    
+    return allOptions
+  }
+
+  const options = parseItemsIntoOptions(data.categories, data.items)
+
+  const handleChange = (event) => {
+    setSelectedOptions(event)
+  }
+
+  const getSelectedItems = (selectedElements) => {
+    const selectedItems = []
+    for (let i = 0; i < selectedElements.length; i++) {
+      if (selectedElements[i].nodeName === 'INPUT' && selectedElements[i].name === 'items') {
+        const selectedItem = data.items.filter(item => item.id === parseInt(selectedElements[i].value))
+        selectedItems.push(selectedItem[0])
+      }
+    }
+    return selectedItems
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const selectedItems = getSelectedItems(event.target.elements)
+    setItems(selectedItems)
+    willTriggerNotification(true)
+  }
+
+  return (
+      <form onSubmit={handleSubmit}>
         <Select
-            isMulti
-            name="items"
-            options={optionss}
-            className="basic-multi-select"
-            classNamePrefix="select"
-            instanceId="postType"
-            onChange={handleChange}
+          isMulti
+          name="items"
+          options={options}
+          className="basic-multi-select"
+          classNamePrefix="select"
+          instanceId="postType"
+          onChange={handleChange}
         />
         <Flex flexDirection="column" justifyContent="center" alignItems="center">
-            <Button backgroundColor="#319795" color="white" my="5" type="submit" isDisabled={listItems === undefined ? true : false}>
+            <Button backgroundColor="#319795" color="white" my="5" type="submit" isDisabled={selectedOptions.length === 0}>
                 Submit
             </Button>
         </Flex>
-        </form>
+      </form>
     );
 };
 
-export default MultiSelect;
+export default MultiSelect
--- client/pages/recycle-and-reuse.js
+++ client/pages/recycle-and-reuse.js
@@ -20,7 +20,9 @@ import Additem from "../components/recycleAndReuseComponents/AddItem";
 import MultiSelect from "../components/recycleAndReuseComponents/MultiSelect";
 import Geolocation from "../components/recycleAndReuseComponents/Geolocation";
 import ItemList from '../components/recycleAndReuseComponents/ItemList';
+import VerifyItem from "../components/recycleAndReuseComponents/VerifyItem";
 // import Router from 'next//router';
+import Link from 'next/link'
 import axios from "axios";
 import { useState, useRef } from "react";
 import urlcat from "urlcat";
@@ -30,101 +32,98 @@ import { options } from "../../mockData/data";
 
 export async function getStaticProps() {
   //   const url = urlcat(process.env.SERVER_URL, "/api/items");
-  const url = urlcat("https://api.npoint.io", "a8416aa207861acb363d");
+  const url = urlcat("https://api.npoint.io", "324727202a8ce24af12f");
   try {
     const data = await axios.get(url);
     return {
       props: {
-        options: data.data,
+        data: data.data,
       },
     };
   } catch (error) {
-    return {
-      props: {
-        options,
-      },
-    };
+    // TODO: handle error in a better way
+    console.error(error)
   }
 }
 
-function RecycleAndReuse({ options }) {
-    const [selectedItems, updateSelectedItems] = useState();
-    const [tabIndex, updateTabIndex] = useState(0);
-    const [didSelectItems, willTriggerNotification] = useState(false);
+function RecycleAndReuse({ data }) {
+  const [items, setItems] = useState({ input: [] })
+  const [tabIndex, setTabIndex] = useState(0)
+  const [didSelectItems, willTriggerNotification] = useState(false);
 
-    // Dialog box for Confirmation
-    const cancelRef = useRef();
-    const onClose = () => willTriggerNotification(false);
-    const proceedNextTab = () => {
-        updateTabIndex(1);
-        onClose();
-    }
+  // Dialog box for Confirmation
+  const cancelRef = useRef();
+  const onClose = () => willTriggerNotification(false);
+  const navigateToVerifyItem = () => {
+      setTabIndex(1);
+      onClose();
+  }
 
-    return (
-        <Center>
-            <AlertDialog isOpen={didSelectItems} leastDestructiveRef={cancelRef} onClose={onClose}>
-                <AlertDialogOverlay>
-                    <AlertDialogContent>
-                        <AlertDialogHeader fontSize='lg' fontWeight='bold'>
-                            Confirmation
-                        </AlertDialogHeader>
+  return (
+    <Center>
+      <AlertDialog isOpen={didSelectItems} leastDestructiveRef={cancelRef} onClose={onClose}>
+          <AlertDialogOverlay>
+              <AlertDialogContent>
+                  <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+                      Confirmation
+                  </AlertDialogHeader>
 
-                        <AlertDialogBody>
-                            Is every item listed? You cannot return to this page to add more items for this round!
-                        </AlertDialogBody>
+                  <AlertDialogBody>
+                      Is every item listed? You cannot return to this page to add more items for this round!
+                  </AlertDialogBody>
 
-                        <AlertDialogFooter>
-                        <Button ref={cancelRef} onClick={onClose}>
-                            Cancel
-                        </Button>
-                        <Button colorScheme='green' onClick={proceedNextTab} ml={3}>
-                            Confirm
-                        </Button>
-                        </AlertDialogFooter>
-                    </AlertDialogContent>
-                </AlertDialogOverlay>
-            </AlertDialog>
-            <Box w="40vw">
-                {/* isLazy prop used to defer rendering each tab until that tab is selected */}
-                <Tabs
-                w="40vw"
-                colorScheme="teal"
-                // variant="enclosed-colored"
-                id="tabs-3--tab--1"
-                isLazy
-                index={tabIndex}
-                >
-                    <TabList>
-                        <Tab isDisabled={tabIndex != 0}>
-                            <AddIcon />
-                            Add Item
-                        </Tab>
-                        {/* <Tab isDisabled={hasNoItems(items)}> */}
-                        <Tab isDisabled={selectedItems && tabIndex == 1 ? selectedItems.count > 0 ? true : false : true}>
-                            <InfoOutlineIcon />
-                            Item List
-                        </Tab>
-                        <Tab isDisabled={selectedItems && tabIndex == 2 ? selectedItems.count > 0 ? true : false : true}>
-                            <DeleteIcon />
-                            Dispose Items
-                        </Tab>
-                    </TabList>
-                    <TabPanels>
-                        <TabPanel>
-                            <Additem />
-                            <MultiSelect options={options} updateTabIndex={updateTabIndex} updateSelectedItems={updateSelectedItems} willTriggerNotification={willTriggerNotification} />
-                        </TabPanel>
-                        <TabPanel>
-                            <ItemList selectedItems={selectedItems} />
-                            </TabPanel>
-                        <TabPanel>
-                            <Geolocation />
-                        </TabPanel>
-                    </TabPanels>
-                </Tabs>
-            </Box>
-        </Center>
-    );
+                  <AlertDialogFooter>
+                  <Button ref={cancelRef} onClick={onClose}>
+                      Cancel
+                  </Button>
+                  <Button colorScheme='green' onClick={navigateToVerifyItem} ml={3}>
+                      Confirm
+                  </Button>
+                  </AlertDialogFooter>
+              </AlertDialogContent>
+          </AlertDialogOverlay>
+      </AlertDialog>
+      <Box w="40vw">
+        {/* isLazy prop used to defer rendering each tab until that tab is selected */}
+        <Tabs
+          w="40vw"
+          colorScheme="teal"
+          // variant="enclosed-colored"
+          id="tabs-3--tab--1"
+          isLazy
+          index={tabIndex}
+          onChange={(index) => setTabIndex(index)}
+        >
+          <TabList>
+            <Tab isDisabled={tabIndex !== 0}>
+              <AddIcon />
+              Add Item
+            </Tab>
+            <Tab isDisabled={tabIndex !== 1}>
+              <InfoOutlineIcon />
+              Item List
+            </Tab>
+            <Tab isDisabled={tabIndex !== 2}>
+              <DeleteIcon />
+              Dispose Items
+            </Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Additem />
+              <MultiSelect data={data} setItems={setItems} willTriggerNotification={willTriggerNotification} />
+            </TabPanel>
+            <TabPanel>
+              <VerifyItem items={items} setItems={setItems} navigateToTakeAction={() => setTabIndex(2)} />
+            </TabPanel>
+            <TabPanel>
+              <Geolocation />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Center>
+  );
 }
 
 export default RecycleAndReuse;
