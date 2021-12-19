a3cb52c2b575271f56809be6424c11c62c9750b1
Ishneet0710 <ishneetsingh0710@gmail.com>
2021-12-19 10:50:56 +0800

    Finished Implementation of Serializers for the DRF

--- server/recycleapp/serializers.py
+++ server/recycleapp/serializers.py
@@ -23,3 +23,40 @@ class BlueBinRecyclableSerializer(serializers.ModelSerializer):
     class Meta:
         model = CallForCollection
         fields = ('id', 'name', 'items_collected', )
+
+
+class NonBlueBinRecyclableSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = n
+        fields = ('id', 'category', 'call_for_collection', 'in_good_condition', 'in_need_of_repair', 'spoilt_beyond_repair', )
+
+
+class GeneralWaste(serializers.ModelSerializer):
+    class Meta:
+        model = g
+        fields = ('id', 'category', 'reason', 'suggestion', )
+
+
+class ChoicesField(serializers.Field):
+    def __init__(self, choices, **kwargs):
+        self._choices = choices
+        super(ChoicesField, self).__init__(**kwargs)
+
+    def to_representation(self, obj):
+        return self._choices[obj]
+
+    def to_internal_value(self, data):
+        return getattr(self._choices, data)
+
+
+class ReuseChannel(serializers.ModelSerializer):
+    channel_of_reuse = ChoicesField(choices=ReuseChannel.CHANNEL_CHOICES)
+
+    class Meta:
+        model = ReuseChannel
+
+
+class PhysicalChannel(serializers.ModelSerializer):
+    class Meta:
+        model = PhysicalChannel
+        fields = ('id', 's/n', 'name_of_organisation', 'address', 'blocknumber', 'building_name', 'postcode', 'operating_hours', 'contact', 'website', 'category', 'channel_of_reuse', 'remarks', )
