9fb91da49a46911c3fbcb017b63e6a8c8cd0e687
joanneong <joanneongcf@u.nus.edu>
2022-01-05 22:52:36 +0800

    fix: update api endpoint url to prevent duplicate /api

--- server/recyclegowhere/urls.py
+++ server/recyclegowhere/urls.py
@@ -25,6 +25,5 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,
         {'document_root':       settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve,
-        {'document_root': settings.STATIC_ROOT}),
-    path('api/', include('recycleapp.urls')),
+        {'document_root': settings.STATIC_ROOT})
 ]
