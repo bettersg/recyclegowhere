e11bc90395606d9a725b19f46f645d849d6df35d
ryhuz <shawn.wee90@hotmail.com>
2022-03-17 00:06:19 +0800

    fix/fix React imports

--- client/components/recycleAndReuseComponents/AddItemDialog.js
+++ client/components/recycleAndReuseComponents/AddItemDialog.js
@@ -1,46 +1,55 @@
-import { React, useRef } from "react";
+import React, { useRef } from "react";
 import {
-    AlertDialog,
-    AlertDialogOverlay,
-    AlertDialogContent,
-    AlertDialogHeader,
-    AlertDialogBody,
-    AlertDialogFooter,
-    Button
+	AlertDialog,
+	AlertDialogOverlay,
+	AlertDialogContent,
+	AlertDialogHeader,
+	AlertDialogBody,
+	AlertDialogFooter,
+	Button,
 } from "@chakra-ui/react";
 
 const AddItemDialog = ({ setNextStep, didSelectItems, willTriggerDialog }) => {
-    const cancelRef = useRef();
-    const onClose = () => willTriggerDialog(false);
-    const navigateToVerifyItem = () => {
-        setNextStep();
-        onClose();
-    };
+	const cancelRef = useRef();
+	const onClose = () => willTriggerDialog(false);
+	const navigateToVerifyItem = () => {
+		setNextStep();
+		onClose();
+	};
 
-    return (
-        <AlertDialog isOpen={didSelectItems} leastDestructiveRef={cancelRef} onClose={onClose}>
-            <AlertDialogOverlay>
-                <AlertDialogContent>
-                    <AlertDialogHeader fontSize='lg' fontWeight='bold'>
-                        Confirmation
-                    </AlertDialogHeader>
+	return (
+		<AlertDialog
+			isOpen={didSelectItems}
+			leastDestructiveRef={cancelRef}
+			onClose={onClose}
+		>
+			<AlertDialogOverlay>
+				<AlertDialogContent>
+					<AlertDialogHeader fontSize="lg" fontWeight="bold">
+						Confirmation
+					</AlertDialogHeader>
 
-                    <AlertDialogBody>
-                        Is every item listed? You cannot return to this page to add more items for this round!
-                    </AlertDialogBody>
+					<AlertDialogBody>
+						Is every item listed? You cannot return to this page to
+						add more items for this round!
+					</AlertDialogBody>
 
-                    <AlertDialogFooter>
-                    <Button ref={cancelRef} onClick={onClose}>
-                        Cancel
-                    </Button>
-                    <Button colorScheme='green' onClick={navigateToVerifyItem} ml={3} >
-                        Confirm
-                    </Button>
-                    </AlertDialogFooter>
-                </AlertDialogContent>
-            </AlertDialogOverlay>
-        </AlertDialog>
-    );
+					<AlertDialogFooter>
+						<Button ref={cancelRef} onClick={onClose}>
+							Cancel
+						</Button>
+						<Button
+							colorScheme="green"
+							onClick={navigateToVerifyItem}
+							ml={3}
+						>
+							Confirm
+						</Button>
+					</AlertDialogFooter>
+				</AlertDialogContent>
+			</AlertDialogOverlay>
+		</AlertDialog>
+	);
 };
 
 export default AddItemDialog;
