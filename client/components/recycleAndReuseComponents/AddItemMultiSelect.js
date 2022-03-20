f87f515e3fa021a6934d9f77e4cfbafe0508b59b
terenvelan <dev.terenvelan@gmail.com>
2022-03-20 13:11:29 +0800

    fix(multiSelect): select multiple options on multiSelect component
    Improve on multiSelect component:
    - added props from React-select libray to enable multiSelect bar does not close after first option clicked
    - add alert feedback from chakraUI to inform user that they can choose more than one option

--- client/components/recycleAndReuseComponents/AddItemMultiSelect.js
+++ client/components/recycleAndReuseComponents/AddItemMultiSelect.js
@@ -1,11 +1,41 @@
-import { Button, Flex, Text } from "@chakra-ui/react";
-import React from "react";
+import { Button, Flex, Text, Alert, AlertIcon } from "@chakra-ui/react";
+import React, { useEffect } from "react";
 import Select from "react-select";
 
 import { selectStylesForColorModes } from "../DarkModeSwitch";
 
-const AddItemMultiSelect = ({ data, setItems, openDialog }) => {
+const AddItemMultiSelect = ({ data, setItems, willTriggerDialog }) => {
 	const [selectedOptions, setSelectedOptions] = React.useState([]);
+
+	const parseItemsIntoOptions = (items) => {
+		const categories = [];
+		const itemsGroupedByCategory = items.reduce((accumulator, item) => {
+			let itemCategory = item.category;
+			if (!accumulator[itemCategory]) {
+				accumulator[itemCategory] = [];
+				categories.push(itemCategory);
+			}
+			accumulator[itemCategory].push(item);
+			return accumulator;
+		}, {});
+
+		return categories.map((category) => {
+			const itemsInCategory = itemsGroupedByCategory[category];
+			const itemOptions = itemsInCategory.map((itemInCategory) => {
+				const option = {
+					label: itemInCategory.description,
+					value: itemInCategory.id,
+				};
+				return option;
+			});
+
+			const categoryOptions = {
+				label: category,
+				options: itemOptions,
+			};
+			return categoryOptions;
+		});
+	};
 
 	const options = parseItemsIntoOptions(data.items);
 
@@ -15,10 +45,13 @@ const AddItemMultiSelect = ({ data, setItems, openDialog }) => {
 
 	const getSelectedItems = (selectedElements) => {
 		const selectedItems = [];
-		for (const selected of selectedElements) {
-			if (selected.nodeName === "INPUT" && selected.name === "items") {
+		for (let i = 0; i < selectedElements.length; i++) {
+			if (
+				selectedElements[i].nodeName === "INPUT" &&
+				selectedElements[i].name === "items"
+			) {
 				const selectedItem = data.items.filter(
-					(item) => item.id === parseInt(selected.value),
+					(item) => item.id === parseInt(selectedElements[i].value),
 				);
 				selectedItems.push(selectedItem[0]);
 			}
@@ -30,13 +63,21 @@ const AddItemMultiSelect = ({ data, setItems, openDialog }) => {
 		event.preventDefault();
 		const selectedItems = getSelectedItems(event.target.elements);
 		setItems(selectedItems);
-		openDialog();
+		willTriggerDialog(true);
 	};
 
 	return (
 		<form onSubmit={handleSubmit} style={{ width: "100%" }}>
+			{!!selectedOptions && selectedOptions.length ? (
+				<Alert size="1rem" mb="1rem" status="success">
+					<AlertIcon />
+					Please feel free to select more than one item from the list.
+				</Alert>
+			) : null}
+
 			<Select
 				isMulti
+				closeMenuOnSelect={false}
 				name="items"
 				options={options}
 				className="basic-multi-select"
@@ -71,33 +112,3 @@ const AddItemMultiSelect = ({ data, setItems, openDialog }) => {
 };
 
 export default AddItemMultiSelect;
-
-const parseItemsIntoOptions = (items) => {
-	const categories = [];
-	const itemsGroupedByCategory = items.reduce((accumulator, item) => {
-		let itemCategory = item.category;
-		if (!accumulator[itemCategory]) {
-			accumulator[itemCategory] = [];
-			categories.push(itemCategory);
-		}
-		accumulator[itemCategory].push(item);
-		return accumulator;
-	}, {});
-
-	return categories.map((category) => {
-		const itemsInCategory = itemsGroupedByCategory[category];
-		const itemOptions = itemsInCategory.map((itemInCategory) => {
-			const option = {
-				label: itemInCategory.description,
-				value: itemInCategory.id,
-			};
-			return option;
-		});
-
-		const categoryOptions = {
-			label: category,
-			options: itemOptions,
-		};
-		return categoryOptions;
-	});
-};
