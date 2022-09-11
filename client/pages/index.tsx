5fdd32e0f3308cb995955172d1f894abb6d02625
ryhuz <shawn.wee90@hotmail.com>
2022-09-12 00:29:01 +0800

    Fix responsiveness of landing page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,4 +1,4 @@
-import { VStack } from "@chakra-ui/react";
+import { VStack, Container } from "@chakra-ui/react";
 import { Banner, UserInput } from "components/home";
 import { BasePage } from "layouts/BasePage";
 import type { NextPage } from "next";
@@ -6,10 +6,18 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
 	return (
 		<BasePage title="Home" description="Singapore's first recycling planner">
-			<VStack align="initial" mx={25} spacing={30}>
-				<Banner />
-				<UserInput />
-			</VStack>
+			<Container
+				maxW={{
+					base: "full",
+					sm: "container.md",
+				}}
+				padding={0}
+			>
+				<VStack align="initial" mx={25} spacing={30}>
+					<Banner />
+					<UserInput />
+				</VStack>
+			</Container>
 		</BasePage>
 	);
 };
