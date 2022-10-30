deb2bf9d0fb161ed3f63fc50f7b49aa1d4d104e1
Reece Russell <rerussell@protonmail.com>
2022-10-30 16:10:38 +0200

    Added in sheety function to app context, needs more tweaking

--- client/app-context/index.tsx
+++ client/app-context/index.tsx
@@ -1,6 +1,17 @@
 import { createContext, Dispatch, ReactNode, useReducer } from "react";
 import { AppContextReducer } from "./reducer";
 import { AppContextActions, AppContextState } from "./types";
+import axios from "axios";
+
+type Item = {
+	"s/n": number;
+	name: string;
+	category: string;
+};
+
+type SheetyResponse = {
+	"itemsList (final)": Item[];
+};
 
 interface IAppContext {
 	state: AppContextState;
@@ -34,3 +45,31 @@ export const AppContextProvider = ({ children }: { children: ReactNode }) => {
 		</AppContext.Provider>
 	);
 };
+
+async function GetSheetData() {
+	try {
+		const data = await axios.get<SheetyResponse> (
+			"https://api.sheety.co/5a6beb2bb1b380b2c7619d503dbaa632/recycleGoWhereBackend [v2]/itemsList (final)",
+			{
+				headers: {
+					Accept: "application/json",
+				},
+			},
+		);
+
+		const Items = [];
+
+		for (let i = 0; i < data["itemsList (final)"].lenght; i++) {
+			Items.push(data["itemsList (final)"][i])
+		};
+		console.log(Items);
+		return Items;
+		
+	} catch (error) {
+		if (axios.isAxiosError(error)) {
+			return error.message;
+		} else {
+			return "An unexpected error has occured";
+		}
+	}
+}
