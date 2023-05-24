5c5a202b2199ebcc07f52803998238a2233ce5f9
Zhi <neozhixuan@gmail.com>
2023-05-24 17:16:15 +0800

    feature/map-page-basic

--- client/spa-pages/components/MapPage.tsx
+++ client/spa-pages/components/MapPage.tsx
@@ -1,12 +1,29 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Text, Button, Spacer } from "@chakra-ui/react";
 import { BasePage } from "layouts/BasePage";
+import { Container, VStack } from "@chakra-ui/react";
+import { COLORS } from "theme";
 
 export const MapPage = () => {
 	return (
-		<BasePage title="Map" description="Singapore's first recycling planner">
-			<Flex direction="column" align="center" my={23}>
-				<Text>Map Page</Text>
-			</Flex>
+		<BasePage title="Instructions" description="Singapore's first recycling planner">
+			<Container
+				maxW={{
+					base: "full",
+					sm: "container.md",
+				}}
+			>
+				<VStack spacing={30} align="stretch">
+					<Spacer />
+					<Flex gap={5} direction={"row"}>
+						<Button bg={COLORS["gray"][700]} color={COLORS.white} flex="1" py="3">
+							Edit
+						</Button>
+						<Button bg={COLORS.Button.primary} color={COLORS.white} flex="1" py="3">
+							View Results
+						</Button>
+					</Flex>
+				</VStack>
+			</Container>
 		</BasePage>
 	);
 };
