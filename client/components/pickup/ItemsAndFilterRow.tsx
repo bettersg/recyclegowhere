ae35e8e9984bea5d71ee1a58837ce5bbfc22fd0b
Li Ying Kwa <kwali5ying@gmail.com>
2023-08-14 16:25:32 +0800

    Complete styling for ItemsAndFilterRow

--- client/components/pickup/ItemsAndFilterRow.tsx
+++ client/components/pickup/ItemsAndFilterRow.tsx
@@ -1,22 +1,23 @@
-import { Icon } from "@chakra-ui/icons";
+import { ChevronDownIcon, ChevronUpIcon, Icon } from "@chakra-ui/icons";
 import { BsFilter } from 'react-icons/bs'
-import { Flex, HStack, IconButton, Spacer, Tag } from "@chakra-ui/react";
-
-const items = [
-    { name: 'Blouse' },
-    { name: 'Shirt' },
-    { name: 'Drink Bottle' },
-    { name: 'Something' }
-]
+import { Box, Flex, HStack, IconButton, Spacer, Tag } from "@chakra-ui/react";
+import { useState } from "react";
+import { useUserInputs } from "hooks/useUserSelection";
 
 const ItemsAndFilterRow = () => {
+    const { items } = useUserInputs();
+    const [isExpanded, setIsExpanded] = useState(false);
+
     return (
         <Flex px={4} gap={4}>
-            <HStack h='48px' flexWrap='wrap' flexGrow={1} spacing={2} px={4} py={3} border='1px' borderColor='gray.200' borderRadius='md' overflow='hidden'>
-                {items.map((item) => (
-                    <Tag flexShrink={0} size='sm' key={item.name} borderRadius='full' colorScheme="blackAlpha">{item.name}</Tag>
-                ))}
-            </HStack>
+            <Flex justifyContent='space-between' flexGrow={1} border='1px' borderColor='gray.200' borderRadius='md'>
+                <Flex h={isExpanded ? '' : '48px'} flexWrap='wrap' rowGap={4} columnGap={2} px={4} py={3} overflow={isExpanded ? '' : 'hidden'}>
+                    {items.map((item) => (
+                        <Tag flexShrink={0} size='sm' key={item.name} borderRadius='full' colorScheme="blackAlpha">{item.name}</Tag>
+                    ))}
+                </Flex>
+                <Icon as={isExpanded ? ChevronUpIcon : ChevronDownIcon} boxSize={6} mr={4} my={3} onClick={() => setIsExpanded(!isExpanded)} />
+            </Flex>
             <IconButton
                 icon={<Icon as={BsFilter} />}
                 variant='outline'
