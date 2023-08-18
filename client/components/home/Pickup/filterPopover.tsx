e2be1f2e320e91bcec256926865f24628e23b693
Zhi <neozhixuan@gmail.com>
2023-08-18 16:34:46 +0800

    Clean + Typescript Fix
    - Removed unnecessary code
    - Fixed ref typing

--- client/components/home/Pickup/filterPopover.tsx
+++ client/components/home/Pickup/filterPopover.tsx
@@ -1,75 +1,66 @@
-import { Button, Flex, Heading, Spacer, Box, ButtonGroup } from "@chakra-ui/react";
-import { ArrowBackIcon, SearchIcon, UpDownIcon } from "@chakra-ui/icons";
-import { Dispatch, SetStateAction, useState, useRef} from "react";
+import { Button, Box } from "@chakra-ui/react";
+import { UpDownIcon } from "@chakra-ui/icons";
+import { useState, useRef } from "react";
 import {
-    Popover, PopoverTrigger, PopoverContent, PopoverHeader, PopoverBody, PopoverFooter, PopoverArrow, PopoverCloseButton,
-    PopoverAnchor, } from '@chakra-ui/react';
+	Popover,
+	PopoverTrigger,
+	PopoverContent,
+	PopoverHeader,
+	PopoverBody,
+	PopoverCloseButton,
+} from "@chakra-ui/react";
+// import { RefObject } from "react";
+// import { FocusableElement } from "@chakra-ui/utils";
 import MarkedSlider from "./slider";
 
+const FilterButton = () => {
+	const initRef = useRef<HTMLElement | null>(null); // Specify the correct type for initRef
 
-const FilterButton= () => {
+	const [priceValue, setPriceValue] = useState(100);
+	const [quantityValue, setQuantityValue] = useState(100);
 
-    const initRef = useRef();
+	const PriceManager = (val: number) => {
+		setPriceValue(val);
+	};
+	const QuantityManager = (val: number) => {
+		setQuantityValue(val);
+	};
 
-    const [priceValue, setPriceValue] = useState(100);
-    const [quantityValue, setQuantityValue] = useState(100);
-    
-    const PriceManager = (val) => {
-        setPriceValue(val);
-    }
-    const QuantityManager = (val) => {
-        setQuantityValue(val);
-    }
-    const exportedPrice = () =>{
-        return priceValue;
-    }
-    const exportedQuantity = () => {
-        return quantityValue;
-    }
+	return (
+		<Popover initialFocusRef={initRef}>
+			{({ isOpen, onClose }) => (
+				<>
+					<PopoverTrigger>
+						<Button leftIcon={<UpDownIcon />} colorScheme={"teal"} size={"md"}>
+							Filter
+						</Button>
+					</PopoverTrigger>
+					<PopoverContent>
+						<PopoverHeader>Filter Results</PopoverHeader>
+						<PopoverCloseButton />
+						<PopoverBody p={5}>
+							<Box>
+								Max Price
+								<MarkedSlider value={priceValue} onSliderChange={PriceManager} />
+							</Box>
+							<br />
+							<Box>
+								Min Quantity
+								<MarkedSlider
+									value={quantityValue}
+									onSliderChange={QuantityManager}
+								/>
+							</Box>
 
-    return (
-        <Popover
-            initialFocusRef={initRef}>
-            {({isOpen, onClose}) => (
-                <>
-                    <PopoverTrigger>
-                        <Button
-                            leftIcon={<UpDownIcon />}
-                            colorScheme={"teal"}
-                            size={"md"}>
-                            Filter
-                        </Button>
-                    </PopoverTrigger>
-                    <PopoverContent>
-                        <PopoverHeader>
-                            Filter Results
-                        </PopoverHeader>
-                        <PopoverCloseButton />
-                        <PopoverBody>
-                            <Box>
-                                Max Price
-                                <MarkedSlider
-                                    value={priceValue} onSliderChange={PriceManager}/>
-                            </Box>
-                            <br />
-                            <Box>
-                                Min Quantity
-                                <MarkedSlider 
-                                    value={quantityValue} onSliderChange={QuantityManager}/>
-                            </Box>
-
-                            <Button
-                                mt={4} colorScheme="teal" onClick={onClose} ref={initRef}>
-                                    Apply
-                            </Button>
-
-                        </PopoverBody>
-
-                    </PopoverContent>
-                </>
-            )}
-        </Popover>
-    );
-}
+							<Button mt={4} colorScheme="teal" onClick={onClose} ref={initRef}>
+								Apply
+							</Button>
+						</PopoverBody>
+					</PopoverContent>
+				</>
+			)}
+		</Popover>
+	);
+};
 
 export default FilterButton;
