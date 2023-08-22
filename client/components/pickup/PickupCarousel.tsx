72054d4d80dead7e4721c62e60cc1355d006d177
Zhi <neozhixuan@gmail.com>
2023-08-22 12:49:49 +0800

    Placed number onto image with responsiveness
    - Placed a test number for the first image, and the actual number on the second

--- client/components/pickup/PickupCarousel.tsx
+++ client/components/pickup/PickupCarousel.tsx
@@ -1,5 +1,5 @@
 import { Box, Text } from "@chakra-ui/react";
-import { useBreakpointValue } from "@chakra-ui/react";
+import { useBreakpointValue, useMediaQuery } from "@chakra-ui/react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import styles from "components/pickup/carousel.module.css";
@@ -13,18 +13,17 @@ const PickupCarousel = ({ minDist }: { minDist: number }) => {
 		{
 			url: useBreakpointValue({
 				base: "/headerBanner1mobile.png",
-				md: "/headerBanner1desktop.png",
+				sm: "/headerBanner1desktop.png",
 			}),
-			number: 20,
 		},
 		{
 			url: useBreakpointValue({
 				base: "/headerBanner2mobile.png",
-				md: "/headerBanner2desktop.png",
+				sm: "/headerBanner2desktop.png",
 			}),
-			distance: "20m",
 		},
 	];
+	const [isMobile] = useMediaQuery("(max-width: 30em)");
 
 	return (
 		<Box className={styles.carouselbox} px={4} h={40}>
@@ -32,7 +31,7 @@ const PickupCarousel = ({ minDist }: { minDist: number }) => {
 				showThumbs={false}
 				showStatus={false}
 				showArrows={false}
-				autoPlay
+				// autoPlay
 				infiniteLoop
 				interval={SLIDES_INTERVAL_TIME}
 				renderIndicator={(clickHandler, isSelected, index) => {
@@ -47,24 +46,43 @@ const PickupCarousel = ({ minDist }: { minDist: number }) => {
 				}}
 			>
 				{slides.map((slide, index) => (
-					<div key={index} style={{ position: "relative" }}>
+					<Box key={index} minWidth={"100%"} style={{ position: "relative" }}>
 						<Image src={slide.url} alt={slide.url} />
 						<div
 							style={{
 								position: "absolute",
-								bottom: index === 0 ? "1%" : "46%", // Adjust the positioning as needed
-								left: index === 0 ? "1%" : "22.5%", // Adjust the positioning as needed
+								bottom: isMobile
+									? index === 0
+										? "46%"
+										: "53%"
+									: index === 0
+									? "48%"
+									: "48.5%", // Adjust the positioning as needed
+								left: isMobile
+									? index === 0
+										? "56%"
+										: "30.5%"
+									: index === 0
+									? "71%"
+									: "21%", // Adjust the positioning as needed
 								color: "black",
 							}}
 						>
-							{index === 0 ? (
-								<Text>Test</Text>
-							) : (
-								<Text> {(minDist * 1000).toFixed(0)}m</Text>
-							)}{" "}
+							<Box w="70px" alignContent={"center"}>
+								{" "}
+								{index === 0 ? (
+									<Text fontSize={["13px", "13px", "20px"]} as={"b"}>
+										TEST
+									</Text>
+								) : (
+									<Text fontSize={["13px", "13px", "20px"]} as={"b"}>
+										{(minDist * 1000).toFixed(0)}m
+									</Text>
+								)}{" "}
+							</Box>
 							{/* Display slide number */}
 						</div>
-					</div>
+					</Box>
 				))}
 			</Carousel>
 		</Box>
