af37c6e73b16a1fd34b248c32910e272ea0eb6e2
Nipuna Gunathilake <nipuna777@gmail.com>
2023-10-02 22:49:36 +0800

    remove unused chakra-ui properties

--- client/components/map/Marker/MarkerIconWrapper.tsx
+++ client/components/map/Marker/MarkerIconWrapper.tsx
@@ -20,62 +20,24 @@ const MarkerIconWrapper = ({
 	category,
 }: Partial<CustomMarkerProps>) => {
 	return (
-		<Box position={"relative"} padding={0} margin={0} className="inline-flex ">
-			{label && (
-				<Box
-					position={"absolute"}
-					rounded={"full"}
-					className="-inset-2 opacity-40"
-					style={{ background: "rgba(0,0,0,0)" }}
-				/>
-			)}
+		<Box>
+      {IconFC &&
+        (label ? (
+          <IconFC className="cluster-marker-size" viewBox="0 0 60 83" />
+        ) : (
+          <IconFC className="marker-size" viewBox="0 0 50 70" />
+        ))}
+      {label && (
+        <Text className="marker-text">{label}</Text>
+      )}
 
-			<Box
-				padding={2}
-				rounded={"full"}
-				position={"relative"}
-				className="inline-block"
-				style={{ background: "rgba(0,0,0,0)" }}
-			>
-				{IconFC &&
-					(label ? (
-						<IconFC className="cluster-marker-size" viewBox="0 0 60 83" />
-					) : (
-						<IconFC className="marker-size" viewBox="0 0 50 70" />
-					))}
-				{label && (
-					<Box
-						position={"relative"}
-						top={2}
-						// right={-2}
-						border={-2}
-						borderColor={"white"}
-						rounded={"full"}
-						height={8}
-						width={20}
-						alignItems={"center"}
-						justifyContent={"end"}
-						paddingTop={1}
-						backgroundColor={color}
-					>
-						<Text className="marker-text">{label}</Text>
-					</Box>
-				)}
-
-				{category && (
-					<Image
-						className="marker-image"
-						src={`/icons/${category}.png`}
-						alt={`${category} icon`}
-					/>
-				)}
-			</Box>
-			<Box
-				position={"absolute"}
-				rounded={"full"}
-				boxShadow={"md"}
-				className={`${label ? "-inset-2" : "-inset-1"}`}
-			/>
+      {category && (
+        <Image
+          className="marker-image"
+          src={`/icons/${category}.png`}
+          alt={`${category} icon`}
+        />
+      )}
 		</Box>
 	);
 };
