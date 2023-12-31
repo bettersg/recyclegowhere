8779ad7d4ecb17a38dbcba9951de424a87d7cff6
Zhi <neozhixuan@gmail.com>
2023-12-31 23:10:03 +0800

    🛠 v1.0.2 - GTM Support
    - Added google tag manager for hotjar

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { AppContextProvider } from "app-context";
 import type { AppProps } from "next/app";
+import React, { useEffect } from "react";
+import TagManager from "react-gtm-module";
 import "leaflet/dist/leaflet.css";
 import "public/Marker.css";
 const theme = extendTheme({
@@ -11,7 +13,14 @@ const theme = extendTheme({
 	},
 });
 
+const tagManagerArgs = {
+	gtmId: "GTM-TWC8D9H",
+};
+
 function MyApp({ Component, pageProps }: AppProps) {
+	useEffect(() => {
+		TagManager.initialize(tagManagerArgs);
+	}, []);
 	return (
 		<ChakraProvider theme={theme}>
 			<AppContextProvider>
